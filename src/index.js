ed329c8f47023a82d4089bc60f7d50826986385f
xingorg1 <xing.org1@outlook.com>
2021-11-25 15:10:38 +0800

    feat: create action

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const dayjs = require("dayjs");
+
+(function main() {
+  const token = core.getInput("token");
+  const octokit = github.getOctokit(token);
+
+  createIssue(octokit);
+})();
+
+function createIssue(octokit) {
+  octokit.rest.issues.create({
+    owner: "xingorg1",
+    repo: "leetcodeRank",
+    title: getTitle(),
+    body: getBody(),
+  });
+}
+
+
+function getTitle() {
+  return `【每日打卡】 ${getDate()}`;
+}
+
+function getBody() {
+  return "⛽️ 加油";
+}
+
+function getDate() {
+  // 运行环境是 UTC 时区
+  // 需要转换成 中国时区
+  // 中国时区 = UTC时区 + 8小时
+  return dayjs().add("8", "hour").format("YYYY-MM-DD");
+}

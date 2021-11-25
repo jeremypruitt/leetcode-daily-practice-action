5321c53681189ae0c7cab6f75ac553c9716fecd5
xingorg1 <xing.org1@outlook.com>
2021-11-25 18:32:38 +0800

    polish: test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const { Octokit } = require("octokit");
-const dayjs = require("dayjs");
+// const dayjs = require("dayjs");
 
 
 const token = core.getInput("token");
@@ -11,17 +11,17 @@ const octokit = new Octokit({
 octokit.rest.issues.create({
   owner: "xingorg1",
   repo: "leetcode-daily-practice-action",
-  title: `【每日打卡】 ${ getDate() }`,
-  body: getBody(),
+  title: `【每日打卡】`,
+  body: "加油",
 });
 
-function getBody() {
-  return "加油";
-}
+// function getBody() {
+//   return "加油";
+// }
 
-function getDate() {
-  // 运行环境是 UTC 时区
-  // 需要转换成 中国时区
-  // 中国时区 = UTC时区 + 8小时
-  return dayjs().add("8", "hour").format("YYYY-MM-DD");
-}
+// function getDate() {
+//   // 运行环境是 UTC 时区
+//   // 需要转换成 中国时区
+//   // 中国时区 = UTC时区 + 8小时
+//   return dayjs().add("8", "hour").format("YYYY-MM-DD");
+// }
